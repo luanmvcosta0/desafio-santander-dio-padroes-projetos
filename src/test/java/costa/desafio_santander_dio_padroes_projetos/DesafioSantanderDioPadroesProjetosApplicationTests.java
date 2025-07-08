3c468f26ec3ccbd31729fca5b72aa31a3372955a
Luan <luanmvcosta@gmail.com>
2025-07-08 14:59:29 -0300

    first commit

---
+++ src/test/java/costa/desafio_santander_dio_padroes_projetos/DesafioSantanderDioPadroesProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package costa.desafio_santander_dio_padroes_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSantanderDioPadroesProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
